f3644add6b56d26254e25d9f628f9969f7207dd8
rjptechsolutions <rjptechsolutions>
2020-12-12 18:50:46 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import arrayWords from '../utils/words';
+import country_name from '../utils/country_name';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'word-genrator';
+
+  words: string = '';
+  limit = 10;
+
+  handelSlideChange(newLimit: number) {
+    this.limit = newLimit;
+  }
+
+  generate() {
+    this.words = arrayWords.slice(0, this.limit).join(' ');
+  }
+  generat_country() {
+    this.words = country_name.slice(0, this.limit).join(' ');
+  }
 }
